fc0e9940b9dbd4c7d4025950a33dccff997964a7
Maciej Migacz <maciejmigacz@gmail.com>
2015-01-12 01:47:35 +0100

    revert data and commit scripts and grapsh only

---
+++ plot3.R
@@ -0,0 +1,26 @@
+zipFile <- "./data/epc.zip"
+
+if(!file.exists("data")){dir.create("data")}
+if(!file.exists(zipFile)){
+  fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(fileUrl, destfile = zipFile, method = "curl", mode="wb")
+  unzip(zipFile, exdir="./data/")  
+}
+
+epc <- read.csv("./data/household_power_consumption.txt", header=TRUE, sep = ";", na.strings="?")
+epc$Time <- strptime(paste(epc$Date, epc$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+epc$Date <- as.Date(epc$Date, "%d/%m/%Y")
+epc2 <- epc[epc$Date %in% c(as.Date("2007-02-01"), as.Date("2007-02-02")), ]
+
+with(epc2, plot(Time, Sub_metering_1, type = "l", ylab = "Enerty sub metering", xlab = NA))
+with(epc2, points(Time, Sub_metering_2, type = "l", col = "red"))
+with(epc2, points(Time, Sub_metering_3, type = "l", col = "blue"))
+legend("topright", lty=c(1,1), lwd=c(2.5,2.5), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+dev.print(png, file = "plot3.png", width = 480, height = 480)
+dev.off()
+
+rm(zipFile)
+rm(epc)
+rm(epc2)
+
